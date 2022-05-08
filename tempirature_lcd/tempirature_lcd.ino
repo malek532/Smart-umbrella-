85bc39c275d16ced98084a702da89636ec7faccc
MALEK ATTIA <61604270+malek532@users.noreply.github.com>
2022-05-08 21:13:57 +0200

    Update tempirature_lcd.ino

--- tempirature_lcd/tempirature_lcd.ino
+++ tempirature_lcd/tempirature_lcd.ino
@@ -1,11 +1,4 @@
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library Example
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
 
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
@@ -14,24 +7,20 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHTPIN 2     // Digital pin connected to the DHT sensor 
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
+#define DHTPIN 2  
 
-// Uncomment the type of sensor in use:
-#define DHTTYPE    DHT11     // DHT 11
-//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
-// See guide for details on sensor wiring and usage:
-//   https://learn.adafruit.com/dht/overview
+#define DHTTYPE    DHT11     
+
+
+
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
 uint32_t delayMS;
 
 void setup() {
-  lcd.init();                      // initialize the lcd 
+  lcd.init();                     
   lcd.init();
   Serial.begin(9600);
   // Initialize device.
@@ -59,7 +48,6 @@ void setup() {
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
-  // Set delay between sensor readings based on sensor details.
   delayMS = sensor.min_delay / 1000;
 }
 
