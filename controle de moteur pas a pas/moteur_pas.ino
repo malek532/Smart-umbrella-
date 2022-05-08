af8cf0db39a263742226d93af769752700f4bcda
MALEK ATTIA <61604270+malek532@users.noreply.github.com>
2022-05-08 21:08:36 +0200

    Update moteur_pas.ino

--- controle de moteur pas a pas/moteur_pas.ino
+++ controle de moteur pas a pas/moteur_pas.ino
@@ -1,33 +1,19 @@
-/*
-  Stepper Motor Demonstration 4
-  Stepper-Demo4.ino
-  Demonstrates NEMA 17 Bipolar Stepper with A4988 Driver
 
-  DroneBot Workshop 2018
-  https://dronebotworkshop.com
-*/
 
-// Define Constants
 
-// Connections to A4988
 const int dirPin = 4;  // Direction
 const int stepPin = 2; // Step
 
-// Motor steps per rotation
-
  
 void setup() {
-  
-  // Setup the pins as Outputs
+ 
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
 }
 void loop() {
   
-  // Set motor direction clockwise
   digitalWrite(dirPin,LOW); 
   
-  // Spin motor one rotation slowly
   for(int x = 0; x < 500; x++) {
     digitalWrite(stepPin,HIGH); 
     delayMicroseconds(1000); 
